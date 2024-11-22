9f24e7a876a34fdc5c59813935f23ca849c93883
nicolaskeller <nicolas.keller@student.unisg.ch>
2024-11-22 11:09:24 +0100

    refactored robot class

--- robot.py
+++ robot.py
@@ -2,15 +2,6 @@ import requests
 import time
 import numpy as np
 
-# idea
-# 1. go down to grab coordinate
-# 2. close gripper
-# 3. go up
-# 4. go to specific location --> has to recognize if piece of clothing has color
-# 5. open gripper for dropping clothes
-# 6. go back to center position
-# 7. repeat
-
 # class for robot controls
 class Robot:
     def __init__(self):
@@ -29,6 +20,12 @@ class Robot:
         self.pitch = None
         self.yaw = None
         self.angleChange = 0
+
+    def _request(self, method, endpoint, data=None):
+        url = self.BASE_URL + endpoint
+        headers = {"Authentication": self.TOKEN} if self.TOKEN else {}
+        response = requests.request(method, url, json=data, headers=headers)
+        return response
 
     def data(self):
         url = self.BASE_URL + "/tcp"
@@ -62,36 +59,6 @@ class Robot:
         else:
             print("Failed to connect to the robot. Status code:", response.status_code)
 
-    def move_to_grab_position(self):
-        url = self.BASE_URL + self.MOVE
-
-        # Include the current z coordinate in the new position
-        target = {
-            "target": {
-                "coordinate": {
-                    "x": 400,
-                    "y": 0,
-                    "z": 300
-                },
-                "rotation": {
-                    "roll": 180,
-                    "pitch": 0,
-                    "yaw": 0
-                }
-            },
-            "speed": 125
-        }
-
-        response = requests.put(url, json=target, headers={"Authentication": self.TOKEN})
-        time.sleep(1)
-        if response.status_code == 200:
-            print(
-                f"Successfully moved the robot")
-            # update positions
-            self.data()
-        else:
-            print("Failed to move the robot. Status code:", response.status_code)
-
     def toggle(self):
         url = self.BASE_URL + self.TOGGLE
         cur = (requests.get(url, headers={"Authentication": self.TOKEN}))
@@ -117,6 +84,33 @@ class Robot:
         else:
             print("Failed to log off from the robot. Status code:", response.status_code)
 
+    def move_to_position(self, x, y, z, roll=180, pitch=0, yaw=0, speed=125):
+        target = {
+            "target": {
+                "coordinate": {"x": x, "y": y, "z": z},
+                "rotation": {"roll": roll, "pitch": pitch, "yaw": yaw},
+            },
+            "speed": speed,
+        }
+        response = self._request("PUT", self.MOVE, target)
+        if response.status_code == 200:
+            print(f"Successfully moved the robot to position: ({x}, {y}, {z})")
+        else:
+            print("Failed to move the robot. Status code:", response.status_code)
+
+    def grab_and_drop(self, drop_position):
+        self.move_to_position(400, 0, 300)  # Move to grab position
+        time.sleep(2)
+        self.move_to_position(400, 0, 15)  # Lower to grab
+        time.sleep(3)
+        self.toggle()  # Grab item
+        time.sleep(3)
+        self.move_to_position(400, 0, 300)  # Lift item
+        time.sleep(2)
+        self.move_to_position(*drop_position)  # Move to drop position
+        time.sleep(3)
+        self.toggle()  # Drop item
+
     def operator(self):
         url = self.BASE_URL + self.CONNECT
         response = requests.get(url)
@@ -140,156 +134,6 @@ class Robot:
         else:
             print("Failed to initialize robot's position. Status code:", response.status_code)
 
-    def grab_and_drop_left(self):
-        self.move_to_grab_position()
-        time.sleep(2)
-        self.lower_for_grab()
-        time.sleep(1)
-        self.toggle()
-        time.sleep(2)
-        self.lift_object()
-        time.sleep(1)
-        self.move_to_drop_position_left()
-        time.sleep(2)
-        self.toggle()
-        print("Grab and drop completed.")
-
-
-    def grab_and_drop_right(self):
-        self.move_to_grab_position()
-        time.sleep(2)
-        self.lower_for_grab()
-        time.sleep(1)
-        self.toggle()
-        time.sleep(2)
-        self.lift_object()
-        time.sleep(1)
-        self.move_to_drop_position_right()
-        time.sleep(2)
-        self.toggle()
-        print("Grab and drop completed.")
-
-    def lower_for_grab(self):
-        url = self.BASE_URL + self.MOVE
-
-        # Include the current z coordinate in the new position
-        target = {
-            "target": {
-                "coordinate": {
-                    "x": 400,
-                    "y": 0,
-                    "z": 15
-                },
-                "rotation": {
-                    "roll": 180,
-                    "pitch": 0,
-                    "yaw": 0
-                }
-            },
-            "speed": 125
-        }
-
-        response = requests.put(url, json=target, headers={"Authentication": self.TOKEN})
-        time.sleep(1)
-        if response.status_code == 200:
-            print(
-                f"Successfully moved the robot")
-            # update positions
-            self.data()
-        else:
-            print("Failed to move the robot. Status code:", response.status_code)
-
-    def lift_object(self):
-        url = self.BASE_URL + self.MOVE
-
-        # Include the current z coordinate in the new position
-        target = {
-            "target": {
-                "coordinate": {
-                    "x": 400,
-                    "y": 0,
-                    "z": 200
-                },
-                "rotation": {
-                    "roll": 180,
-                    "pitch": 0,
-                    "yaw": 0
-                }
-            },
-            "speed": 125
-        }
-
-        response = requests.put(url, json=target, headers={"Authentication": self.TOKEN})
-        time.sleep(1)
-        if response.status_code == 200:
-            print(
-                f"Successfully moved the robot")
-            # update positions
-            self.data()
-        else:
-            print("Failed to move the robot. Status code:", response.status_code)
-
-    def move_to_drop_position_left(self):
-        url = self.BASE_URL + self.MOVE
-
-        # Include the current z coordinate in the new position
-        target = {
-            "target": {
-                "coordinate": {
-                    "x": 400,
-                    "y": 500,
-                    "z": 200
-                },
-                "rotation": {
-                    "roll": 180,
-                    "pitch": 0,
-                    "yaw": 0
-                }
-            },
-            "speed": 125
-        }
-
-        response = requests.put(url, json=target, headers={"Authentication": self.TOKEN})
-        time.sleep(1)
-        if response.status_code == 200:
-            print(
-                f"Successfully moved the robot")
-            # update positions
-            self.data()
-        else:
-            print("Failed to move the robot. Status code:", response.status_code)
-
-
-    def move_to_drop_position_right(self):
-        url = self.BASE_URL + self.MOVE
-
-        # Include the current z coordinate in the new position
-        target = {
-            "target": {
-                "coordinate": {
-                    "x": 400,
-                    "y": -500,
-                    "z": 200
-                },
-                "rotation": {
-                    "roll": 180,
-                    "pitch": 0,
-                    "yaw": 0
-                }
-            },
-            "speed": 125
-        }
-
-        response = requests.put(url, json=target, headers={"Authentication": self.TOKEN})
-        time.sleep(1)
-        if response.status_code == 200:
-            print(
-                f"Successfully moved the robot")
-            # update positions
-            self.data()
-        else:
-            print("Failed to move the robot. Status code:", response.status_code)
-
     def main(self):
         while True:
             print("\nPlease select an option:")
@@ -297,40 +141,26 @@ class Robot:
             print("2. logoff")
             print("3. initialize")
             print("4. Toggle the gripper")
-            print("5. move to grab position")
-            print("6. lower forgrab")
-            print("7. lift")
-            print("8. move to drop position left")
-            print("9. move to drop position right")
-            print("10. Complete drag and drop left")
-            print("11. Complete drag and drop right")
+            print("5. left")
+            print("6. right")
 
             choice = input("Enter your choice (1-11): ")
             if choice == '1':
                 self.connect()
             elif choice == '2':
+                self.initialize()
                 self.log_off()
                 break
             elif choice == '3':
                 self.initialize()
             elif choice == '4':
                 self.toggle()
-            elif choice == '5':
-                self.move_to_grab_position()
-            elif choice == '6':
-                self.lower_for_grab()
-            elif choice == '7':
-                self.lift_object()
-            elif choice == '8':
-                self.move_to_drop_position_left()
-            elif choice == '9':
-                self.move_to_drop_position_right()
-            elif choice == '10':
-                self.grab_and_drop_left()
-            elif choice == '11':
-                self.grab_and_drop_right()
+            elif choice == "5":
+                self.grab_and_drop((400, 500, 300))  # Left drop position
+            elif choice == "6":
+                self.grab_and_drop((400, -500, 300))  # Right drop position
             else:
-                print("Invalid choice. Please select a number between 1 and 8.")
+                print("Invalid choice. Please select a valid option.")
             time.sleep(1)  # Wait for 1 second before processing the next command
 
 # create robot instance and run main()
